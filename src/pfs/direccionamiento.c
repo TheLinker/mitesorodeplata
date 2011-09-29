99d85dd79f9bea372a7e463d5ec572cb28617df0
edgarschmid@live.com.ar <edgarschmid@live.com.ar@8a893867-091e-0b5e-2fe9-50ebfefd8d3d>
2011-09-29 03:35:56 +0000

    separado codigo del pfs fat32_getsectors y fat32_getcluster se fueron al direccionamiento.c

---
+++ src/pfs/direccionamiento.c
@@ -0,0 +1,71 @@
+#include "direccionamiento.h"
+#include "nipc.h"
+#include "pfs.h"
+#include <string.h>
+#include <stdlib.h>
+#include <errno.h>
+
+/**
+ * Obtiene *cantidad* sectores a partir de *sector*
+ * NOTA: el *buffer* debe tener el tamaño suficiente para aceptar los datos
+ *
+ * @sector primer sector a pedir
+ * @cantidad cantidad de sectores a partir de *sector* a pedir (inclusive)
+ * @buffer lugar donde guardar la información recibida.
+ * @return codigo de error o 0 si fue todo bien.
+ */
+uint8_t fat32_getsectors(uint32_t sector, uint32_t cantidad, void *buffer)
+{
+    nipc_packet packet;
+    int i;
+
+    packet.type = nipc_req_packet;
+    packet.len = sizeof(int32_t);
+    memcpy(packet.payload, &sector, sizeof(int32_t));
+
+    //Pide los sectores necesarios
+    for (i = 0 ; i < cantidad ; i++)
+    {
+        nipc_send_packet(&packet, socket);
+        *((int32_t *)packet.payload) += 1;
+    }
+
+    //Espera a obtener todos los sectores pedidos
+    for (i = 0 ; i < cantidad ; i++)
+    {
+        nipc_packet *packet = nipc_recv_packet(socket);
+        int32_t rta_sector;
+        memcpy(&rta_sector, packet->payload, sizeof(int32_t));
+        memcpy(buffer+((rta_sector-sector)*fat.boot_sector.bytes_per_sector), packet->payload+4, fat.boot_sector.bytes_per_sector);
+        free(packet);
+    }
+
+    return 0;
+}
+
+/**
+ * Obtiene un *cluster*
+ * NOTA: el *buffer* debe tener el tamaño suficiente para aceptar los datos
+ *
+ * @cluster cluster a pedir
+ * @buffer lugar donde guardar la información recibida.
+ * @return codigo de error o 0 si fue todo bien.
+ */
+uint8_t fat32_getcluster(uint32_t cluster, void *buffer)
+{
+    //cluster 0 y 1 estan reservados y es invalido pedir esos clusters
+    if(cluster<2) return -EINVAL;
+
+////  SSA=RSC(0x0E) + FN(0x10) * SF(0x24)
+////  LSN=SSA + (CN-2) × SC(0x0D)
+
+    uint32_t logical_sector_number = 0;
+
+    logical_sector_number = fat.system_area_size + (cluster - 2) * fat.boot_sector.sectors_per_cluster;
+
+    fat32_getsectors(logical_sector_number , fat.boot_sector.sectors_per_cluster, buffer);
+
+    return 0;
+}
+
+
