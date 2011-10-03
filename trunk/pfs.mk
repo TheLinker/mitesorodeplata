66a40c4a682693a1f64b963a99b3fc4c85ca45c9
axel.bayerl@gmail.com <axel.bayerl@gmail.com@8a893867-091e-0b5e-2fe9-50ebfefd8d3d>
2011-10-03 01:15:54 +0000

    Modificado y modularizado el Makefile. Ahora modifiquen pfs.mk y/o praid.mk al agregar (o sacar) archivos a compilar.
    PRAID_BIN, PFS_BIN: nombre del ejecutable
    PRAID_SRC, PFS_SRC: archivos a compilar para el proyecto actual (los .c).
    PRAID_INCLUDES, PFS_INCLUDES: (opcional) otros archivos de los que dependen los archivos a compilar (los .h).

---
+++ trunk/pfs.mk
@@ -0,0 +1,11 @@
+PFS_BIN = $(BIN_PATH)/process_file_system
+
+PFS_SRC = $(SRC_PATH)/common/nipc.c \
+          $(SRC_PATH)/pfs/direccionamiento.c \
+          $(SRC_PATH)/pfs/utils.c \
+          $(SRC_PATH)/pfs/pfs.c \
+
+PFS_INCLUDES = $(SRC_PATH)/common/nipc.h \
+               $(SRC_PATH)/pfs/direccionamiento.h \
+               $(SRC_PATH)/pfs/utils.h \
+
