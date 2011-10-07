97329f017c9cdd6b41e9c66201c3d32226cecb21
nando5.89@gmail.com <nando5.89@gmail.com@8a893867-091e-0b5e-2fe9-50ebfefd8d3d>
2011-10-07 23:23:46 +0000

    0 Warnings =)

--- praid.mk
+++ praid.mk
@@ -1,6 +1,7 @@
 PRAID_BIN = $(BIN_PATH)/praid
 
-PRAID_SRC = $(SRC_PATH)/praid1/praid.c
+PRAID_SRC = $(SRC_PATH)/common/nipc.c \
+            $(SRC_PATH)/praid1/praid.c \
 
-PRAID_INCLUDES =  
+PRAID_INCLUDES = $(SRC_PATH)/common/nipc.h \
 
