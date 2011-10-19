2eece3083d65fe810584e823036ae0e90d707b16
edgarschmid@live.com.ar <edgarschmid@live.com.ar@8a893867-091e-0b5e-2fe9-50ebfefd8d3d>
2011-10-19 22:15:25 +0000

    Cambiando direccionamiento en el PFS. Reemplazando llamadas a sectores por llamadas a bloques

--- src/pfs/direccionamiento.h
+++ src/pfs/direccionamiento.h
@@ -4,7 +4,10 @@
 #include <stdint.h>
 #include "pfs.h"
 
-uint8_t fat32_getsectors(uint32_t sector, uint32_t cantidad, void *buffer, fs_fat32_t *fs_tmp);
+#define BLOCK_SIZE 1024
+#define SECTORS_PER_BLOCK BLOCK_SIZE / 512
+
+uint8_t fat32_getblock(uint32_t block, uint32_t cantidad, void *buffer, fs_fat32_t *fs_tmp);
 uint8_t fat32_getcluster(uint32_t cluster, void *buffer, fs_fat32_t *fs_tmp);
 
 #endif //__DIRECCIONAMIENTO_H_
