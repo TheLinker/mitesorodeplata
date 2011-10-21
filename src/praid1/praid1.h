7ea97ec19a97454c366873f0492e49c6ca132e44
nando5.89@gmail.com <nando5.89@gmail.com@8a893867-091e-0b5e-2fe9-50ebfefd8d3d>
2011-10-21 12:26:12 +0000

    Retoques Esteticos!

--- src/praid1/praid1.h
+++ src/praid1/praid1.h
@@ -7,29 +7,29 @@
 #define SIZEBUF 1024
 
 struct mensajeCola{
-	uint32_t type;
-	uint32_t sector;
-	char 	 contenido[512];
+		uint32_t 	type;
+		uint32_t 	sector;
+		char 	 	contenido[512];
 }__attribute__ ((packed));
 
 typedef struct _pedido{
-	char type_pedido;
-	uint32_t sector;
-	struct _pedido *sgte;
+	char 			type_pedido;
+	uint32_t 		sector;
+	struct _pedido 	*sgte;
 } __attribute__ ((packed)) pedido;
 
 typedef struct _disco{
-	char  id[20];
-	uint32_t sock;
-	pthread_t hilo;
-	uint32_t cantidad_pedidos;
-	pedido   *pedidos;
-	struct _disco *sgte;
+	char  			id[20];
+	nipc_socket 	sock;
+	pthread_t 		hilo;
+	uint32_t 		cantidad_pedidos;
+	pedido   		*pedidos;
+	struct _disco 	*sgte;
 } __attribute__ ((packed)) disco;
 
 typedef struct _pfs{
-		uint32_t sock;
-		struct _pfs *sgte;
+		nipc_socket 	sock;
+		struct _pfs 	*sgte;
 } __attribute__ ((packed)) pfs;
 
 #endif //__PRAID1_H_
