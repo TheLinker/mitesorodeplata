0340aa1eb1a2beb626a1a3d65b2e86fdca48f3cc
lucas.pando17@gmail.com <lucas.pando17@gmail.com@8a893867-091e-0b5e-2fe9-50ebfefd8d3d>
2011-12-01 15:44:27 +0000

    El nuevo ppd. Hay q probarlo si funciona. Ahora mapea todo el disco.

---
+++ trunk/src/ppd2/ppd.h
@@ -0,0 +1,68 @@
+#ifndef PPD_H_
+#define PPD_H_
+
+#include <stdio.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "getconfig.h"
+#include "planDisco.h"
+#include "conexionPFS.h"
+#include <signal.h>   
+#include <sys/types.h>   
+#include <sys/socket.h>   
+#include <sys/un.h> 
+#include <pthread.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <semaphore.h>
+#include <unistd.h>
+
+#include <arpa/inet.h>
+
+#define TAM_SECT 512
+#define TAM_PAG 4096
+#define PROTOCOLO 0  
+
+char comando[200], buffer[TAM_SECT], bufferent[TAM_SECT];
+char * pathArch;
+int8_t * dirMap, * dirSect;
+int sect, cantSect, offset;
+
+
+void escucharPedidos(nipc_socket *sock);
+
+void atenderPedido(void);
+
+void leerSect(int sect, nipc_socket sock);
+
+void escribirSect(int, char [TAM_SECT], nipc_socket sock);
+
+int abrirArchivoV(char * pathArch);
+
+void * discoMap (int tam, int dirArch);
+
+void escucharConsola(void);
+
+void atenderConsola(char comando[100], int cliente);
+
+void funcInfo(int cliente);
+
+void funcClean(char *, int cliente);
+
+void funcTrace(char *, int cliente);
+
+void traceSect(int sect, int32_t nextsect, int cliente);
+
+int my_itoa(int val, char* buf);
+
+void errorparam(void);
+
+void msjprueba(nipc_packet *);
+
+void conectarConPraid();
+
+int calcularSector(char structSect[25]);
+
+#endif /* PPD_H_ */
